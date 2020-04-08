caf4ee68a0240bc5103a6b6689691ef7917053b7
sicio87 <karol.sicko@gmail.com>
2020-04-08 22:12:33 +0200

    add eureka client disabled by default in docker-compose environment variables use wait-for-it script changes in docker-compose to work with other services

--- src/main/java/eu/karols/todolist/TodolistApplication.java
+++ src/main/java/eu/karols/todolist/TodolistApplication.java
@@ -2,8 +2,10 @@ package eu.karols.todolist;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class TodolistApplication {
 
     public static void main(String[] args) {
